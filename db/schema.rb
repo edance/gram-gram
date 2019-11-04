06f88bef99213d9ad48aa20aacf405cffe95edb9
Evan Dancer <evancdancer@gmail.com>
2019-11-03 19:56:25 -0800

    Fetch access token from instagram

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_04_004904) do
+ActiveRecord::Schema.define(version: 2019_11_05_043518) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,8 @@ ActiveRecord::Schema.define(version: 2019_11_04_004904) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "instagram_uid"
+    t.string "instagram_access_token"
   end
 
   add_foreign_key "photos", "users"
