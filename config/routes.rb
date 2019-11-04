08bb2a6be6e5d6018e48cacf948a269c29491a0f
Evan Dancer <evancdancer@gmail.com>
2019-11-03 19:34:36 -0800

    Add authorize url for instagram

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  # Static routes
   root to: 'static#home'
-  get '/privacy' => 'static#privacy'
-  get '/terms' => 'static#terms'
+  get '/privacy', to: 'static#privacy'
+  get '/terms', to: 'static#terms'
+
+  # Authentication routes
+  get 'login', to: redirect('/auth/instagram'), as: 'login'
+  get '/auth/:provider', to: 'authentication#authorize'
+  get '/auth/:provider/callback', to: 'authentication#callback'
 end
