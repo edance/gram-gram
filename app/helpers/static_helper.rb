140bd6f685a34f98594c0aff478d436f23a57be5
Evan Dancer <evancdancer@gmail.com>
2019-11-03 16:50:59 -0800

    Add static pages for home, privacy, and terms

---
+++ app/helpers/static_helper.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module StaticHelper
+  def company_name
+    'GramGram'
+  end
+
+  def company_url
+    'https://gramgram.app'
+  end
+
+  def help_email
+    'help@gramgram.app'
+  end
+end
