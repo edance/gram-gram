ade3c0f166a506910d162cba193d4b7ac8786940
Tim <tjones4413@gmail.com>
2019-11-04 20:43:04 -0800

    Respond to PR comments - attr_accessor & ENV variables

--- app/jobs/send_postcard_job.rb
+++ app/jobs/send_postcard_job.rb
@@ -1,26 +1,25 @@
 class SendPostcardJob < ApplicationJob
   queue_as :default
 
-  FRONT_TEMPLATE_ID = 'tmpl_e54af2cab69b793'.freeze
-  BACK_TEMPLATE_ID = 'tmpl_ef2f5ce8783d9d7'.freeze
+  attr_accessor :postcard
 
   def perform(postcard_id)
     @postcard = Postcard.find_by_id(postcard_id)
-    return if @postcard.nil?
+    return if postcard.nil?
 
     lob.postcards.create(
-      description: @postcard.lob_description,
-      to: @postcard.recipient.address,
-      front: FRONT_TEMPLATE_ID,
-      back: BACK_TEMPLATE_ID,
+      description: postcard.lob_description,
+      to: postcard.recipient.address,
+      front: ENV['POSTCARD_FRONT_TEMPLATE_ID'],
+      back: ENV['POSTCARD_BACK_TEMPLATE_ID'],
       merge_variables: merge_variables
     )
   end
 
   def merge_variables
     {
-      photo_url: @postcard.photo.ig_permalink,
-      caption: @postcard.caption
+      photo_url: postcard.photo.ig_permalink,
+      caption: postcard.caption
     }
   end
 
