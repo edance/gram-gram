0afc17a4be0bf9d7c216aae0c5b5ad28588eee7a
Evan Dancer <evancdancer@gmail.com>
2019-11-05 20:45:42 -0800

    Add methods to query instagram api

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -13,8 +13,7 @@ class AuthenticationController < ApplicationController
   end
 
   def callback
-    resp = fetch_access_token
-    attrs = JSON.parse(resp.body)
+    attrs = fetch_access_token
     user = User.find_or_initialize_by(instagram_uid: attrs['user_id'])
     user.update!(instagram_access_token: attrs['access_token'])
     session[:current_user_id] = user.id
@@ -29,12 +28,22 @@ class AuthenticationController < ApplicationController
   end
 
   def fetch_access_token
+    faraday_conn.post(
+      'oauth/access_token',
+      app_id: ENV['INSTAGRAM_ID'],
+      app_secret: ENV['INSTAGRAM_SECRET'],
+      grant_type: 'authorization_code',
+      redirect_uri: 'https://gramgram.app/auth/instagram/callback',
+      code: code
+    ).body
+  end
+
+  def faraday_conn
     url = 'https://api.instagram.com/oauth/access_token'
-    Faraday.post(url,
-                 app_id: ENV['INSTAGRAM_ID'],
-                 app_secret: ENV['INSTAGRAM_SECRET'],
-                 grant_type: 'authorization_code',
-                 redirect_uri: 'https://gramgram.app/auth/instagram/callback',
-                 code: code)
+    Faraday.new(url: url) do |f|
+      f.response :json
+
+      f.adapter Faraday.default_adapter
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,35 @@
 class User < ApplicationRecord
   has_many :photos
   has_many :recipients
+
+  INSTAGRAM_BASE_URL = 'https://graph.instagram.com'
+  USER_FIELDS = %w[id username media_count account_type].freeze
+  MEDIA_FIELDS = %w[caption id media_type media_url permalink thumbnail_url
+                    timestamp username].freeze
+
+  def user_information
+    instagram_client.get(
+      'me',
+      fields: USER_FIELDS.join(','),
+      access_token: instagram_access_token
+    ).body
+  end
+
+  def media
+    instagram_client.get(
+      "#{instagram_uid}/media",
+      fields: MEDIA_FIELDS.join(','),
+      access_token: instagram_access_token
+    ).body
+  end
+
+  private
+
+  def instagram_client
+    @instagram_client ||= Faraday.new(url: INSTAGRAM_BASE_URL) do |f|
+      f.response :json
+
+      f.adapter Faraday.default_adapter
+    end
+  end
 end
