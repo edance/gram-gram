4ca0cae23862b05e94a1fa4fc4bdbe3a00f6577d
Evan Dancer <evancdancer@gmail.com>
2019-11-10 15:21:43 -0800

    Add env variable for instagram redirect uri

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,14 +1,11 @@
 # frozen_string_literal: true
 
 class AuthenticationController < ApplicationController
-  # Change this to localhost to sign in
-  REDIRECT_URI = 'https://gramgram.app/auth/instagram/callback'
-
   def authorize
     base_url = 'https://api.instagram.com/oauth/authorize'
     query = {
       app_id: ENV['INSTAGRAM_ID'],
-      redirect_uri: REDIRECT_URI,
+      redirect_uri: ENV['INSTAGRAM_REDIRECT_URI'],
       scope: 'user_profile,user_media',
       response_type: 'code'
     }
@@ -48,7 +45,7 @@ class AuthenticationController < ApplicationController
                  app_id: ENV['INSTAGRAM_ID'],
                  app_secret: ENV['INSTAGRAM_SECRET'],
                  grant_type: 'authorization_code',
-                 redirect_uri: REDIRECT_URI,
+                 redirect_uri: ENV['INSTAGRAM_REDIRECT_URI'],
                  code: code)
   end
 end
