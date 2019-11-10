6bd16f9d09e73b7b7ecbb3aab96a589ebe364285
Evan Dancer <evancdancer@gmail.com>
2019-11-10 15:25:04 -0800

    Add ig_username to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_10_062936) do
+ActiveRecord::Schema.define(version: 2019_11_10_232302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 2019_11_10_062936) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "instagram_uid"
     t.string "instagram_access_token"
+    t.string "ig_username"
   end
 
   add_foreign_key "photos", "users"
