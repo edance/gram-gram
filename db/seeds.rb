dbef65ac252731fde2e53fabf710795abf98c946
Tim <tjones4413@gmail.com>
2019-11-09 19:03:26 -0800

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ user = User.find_or_create_by(
 )
 
 photo = Photo.find_or_create_by(
-  ig_media_url: 'https://scontent.xx.fbcdn.net/v/t51.2885-15/29415885_2050287158573564_7888298542568046592_n.jpg?_nc_cat=109&_nc_oc=AQkSm4jIjfe-HSC-D6iY5Md3G5O0ABHTqvGPNcZRpeB57Mt5HTyAaM8iNTIhqPmsBEM&_nc_ht=scontent.xx&oh=a96e278dfc5569b87a217fc16eec9aaa&oe=5E401E7A',
+  ig_media_url: 'https://www.fillmurray.com/200/300',
   user: user
 )
 
