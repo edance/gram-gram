a6004d901194af5bdc47fdc2f5215a52fc77f2ed
Evan Dancer <evancdancer@gmail.com>
2019-11-09 22:36:40 -0800

    Allow postcards without a recipient

--- app/models/postcard.rb
+++ app/models/postcard.rb
@@ -1,6 +1,6 @@
 class Postcard < ApplicationRecord
-  belongs_to :recipient
   belongs_to :photo
+  belongs_to :recipient, optional: true
 
   enum status: %i[in_transit in_local_area processed_for_delivery re_routed
                   returned_to_sender]
