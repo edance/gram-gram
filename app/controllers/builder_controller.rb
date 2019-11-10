73d62674a407b09dc6cec9c24f9773cd5674914c
Evan Dancer <evancdancer@gmail.com>
2019-11-09 23:43:46 -0800

    Add builder controller

---
+++ app/controllers/builder_controller.rb
@@ -0,0 +1,85 @@
+class BuilderController < ApplicationController
+  STRIPE_CHARGE_DESCRIPTION = 'GramGram Photo'.freeze
+  RECIPIENT_PARAMS = %i[
+    name
+    address_line1
+    address_line2
+    address_city
+    address_state
+    address_zip
+  ].freeze
+
+  def new
+    photo = current_user.photos.find(params[:photo_id])
+    postcard = Postcard.create(photo: photo)
+
+    redirect_to build_caption_path(postcard)
+  end
+
+  def caption
+    @postcard = postcard
+  end
+
+  def update_caption
+    if postcard.update(caption_params)
+      redirect_to build_recipient_path
+    else
+      render 'caption'
+    end
+  end
+
+  def recipient
+    @postcard = postcard
+  end
+
+  # TODO: select from dropdown
+  def update_recipient
+    recipient = current_user.recipients.new(recipient_params)
+    postcard.recipient = recipient
+
+    if recipient.save && postcard.save
+      redirect_to build_payment_path
+    else
+      render 'recipient'
+    end
+  end
+
+  def payment
+    @postcard = postcard
+  end
+
+  # TODO: add saving of charge
+  def update_payment
+    create_charge
+    flash[:notice] = 'Your photo has been sent!'
+
+    redirect_to root_path
+  end
+
+  private
+
+  def postcard
+    @postcard = current_user.postcards.find(params[:id])
+  end
+
+  def recipient_params
+    params.require(:postcard).require(:recipient).permit(RECIPIENT_PARAMS)
+  end
+
+  def caption_params
+    params.require(:postcard).permit(:caption)
+  end
+
+  def create_charge
+    Stripe::Charge.create(
+      amount: Postcard::PRICE,
+      currency: 'usd',
+      description: STRIPE_CHARGE_DESCRIPTION,
+      source: token
+    )
+  end
+
+  def token
+    params.require(:token)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,16 @@ Rails.application.routes.draw do
   post '/charge' => 'payments#charge'
 
   resources :photos, only: %i[index show]
+  get '/photos/:photo_id/new', to: 'builder#new', as: 'build_new'
+
+  get '/sender/:id/caption', to: 'builder#caption', as: 'build_caption'
+  get '/sender/:id/address', to: 'builder#recipient', as: 'build_recipient'
+  get '/sender/:id/payment', to: 'builder#payment', as: 'build_payment'
+
+  patch '/sender/:id/caption', to: 'builder#update_caption', as: 'update_caption'
+  patch '/sender/:id/address', to: 'builder#update_recipient', as: 'update_recipient'
+  patch '/sender/:id/payment', to: 'builder#update_payment', as: 'update_payment'
+
   get '/photos/:photo_id/send', to: 'postcards#new', as: 'postcard_send'
   post '/photos/:photo_id/send', to: 'postcards#create', as: 'postcard_create'
   resources :postcards, only: %i[index show]
