c8bb1c8c2090c76e6a9ff4477d4fca6c0c9fd7c0
Tim <tjones4413@gmail.com>
2019-11-09 17:08:47 -0800

    Update seeds with latest Photo changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,11 @@
 user = User.find_or_create_by(
   name: 'Grandma',
-  email: 'grandma@aol.com'
+  email: 'grandma@aol.com',
+  instagram_uid: '17841401619133950'
 )
 
 photo = Photo.find_or_create_by(
-  ig_permalink: 'https://www.fillmurray.com/200/300',
+  ig_media_url: 'https://scontent.xx.fbcdn.net/v/t51.2885-15/29415885_2050287158573564_7888298542568046592_n.jpg?_nc_cat=109&_nc_oc=AQkSm4jIjfe-HSC-D6iY5Md3G5O0ABHTqvGPNcZRpeB57Mt5HTyAaM8iNTIhqPmsBEM&_nc_ht=scontent.xx&oh=a96e278dfc5569b87a217fc16eec9aaa&oe=5E401E7A',
   user: user
 )
 
