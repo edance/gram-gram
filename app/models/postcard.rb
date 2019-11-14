54611f3fabf84f9ed473d398bbb1d4c98d6235e8
Tim <tjones4413@gmail.com>
2019-11-13 21:05:45 -0800

    Validate Postcard before sending with Lob

--- app/models/postcard.rb
+++ app/models/postcard.rb
@@ -1,6 +1,7 @@
 class Postcard < ApplicationRecord
   belongs_to :photo
   belongs_to :recipient, optional: true
+  after_commit :send_with_lob_if_sendable
 
   enum status: %i[in_transit in_local_area processed_for_delivery re_routed
                   returned_to_sender]
@@ -9,5 +10,17 @@ class Postcard < ApplicationRecord
 
   def lob_description
     "postcard_id_#{id}"
+  end
+
+  private
+
+  def send_with_lob_if_sendable
+    SendPostcardJob.perform_async(self) if sendable?
+  end
+
+  def sendable?
+    !recipient.nil? &&
+      !photo.nil? &&
+      !stripe_charge_id.nil?
   end
 end
