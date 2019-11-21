c0d2e6c8df5afe69c2bb29425eaf21f1b813bd80
Evan Dancer <evancdancer@gmail.com>
2019-11-21 15:15:35 -0500

    Add uuid as primary key

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_14_031533) do
+ActiveRecord::Schema.define(version: 2019_11_21_201208) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
   create_table "photos", force: :cascade do |t|
