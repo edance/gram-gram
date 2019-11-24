39331546005c7e97752e5ba4850db3e15c76f0e3
Evan Dancer <evancdancer@gmail.com>
2019-11-24 00:11:05 -0500

    Fix bootstrap javascript

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,9 @@ require('@rails/ujs').start();
 require('turbolinks').start();
 require('@rails/activestorage').start();
 require('channels');
+require('popper.js');
+require('jquery');
+require('bootstrap');
 
 require('components');
 
