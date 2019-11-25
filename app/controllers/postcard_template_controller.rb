8016114d34a6ba15ac0c43a6a997dd981c398ebe
Evan Dancer <evancdancer@gmail.com>
2019-11-25 14:47:54 -0500

    Fix layout for front of the postcard

--- app/controllers/postcard_template_controller.rb
+++ app/controllers/postcard_template_controller.rb
@@ -1,3 +1,8 @@
 class PostcardTemplateController < ApplicationController
   layout false
+
+  def front
+    @photo_url = 'https://www.fillmurray.com/150/150.jpg'
+    @caption = 'This is an example'
+  end
 end
