bcb7773a8673d7b8ffdd6882808b4d59c3dd4a7d
Evan Dancer <evancdancer@gmail.com>
2019-12-31 17:41:42 -0500

    Switch photos controller to use devise auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,2 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user
-
-  private
-
-  def current_user
-    @current_user ||= User.find_by_id(cookies.encrypted[:user_id])
-  end
-
-  def redirect_guests
-    return if current_user.present?
-
-    redirect_to root_path
-  end
 end
