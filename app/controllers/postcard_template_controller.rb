e07847fe9d23925b2c38c85a814be5b86eb2f7a3
Evan Dancer <evancdancer@gmail.com>
2020-01-10 22:10:16 -0800

    Add date postcard template

--- app/controllers/postcard_template_controller.rb
+++ app/controllers/postcard_template_controller.rb
@@ -2,6 +2,7 @@ class PostcardTemplateController < ApplicationController
   layout false
 
   def front
+    @postcard = Postcard.order(:created_at).last
     @photo_url = 'https://www.fillmurray.com/150/150.jpg'
     @caption = 'This is an example'
   end
