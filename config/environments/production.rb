9a0b1bef275880f6b36acb729bf1fbc07d5bd8b1
Evan Dancer <evancdancer@gmail.com>
2020-01-13 20:16:13 -0800

    Fix action cable to work under /app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,8 +39,8 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain.
-  # config.action_cable.mount_path = '/app/cable'
-  config.action_cable.url = 'wss://gram-2-grandma.herokuapp.com/app/cable'
+  config.action_cable.mount_path = '/app/cable'
+  # config.action_cable.url = 'wss://gram-2-grandma.herokuapp.com/app/cable'
   config.action_cable.allowed_request_origins = ['https://www.gramgram.app',
                                                  'https://gram-2-grandma.herokuapp.com/']
 
