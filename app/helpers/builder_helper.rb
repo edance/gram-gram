5a51f6d48cfd92b67215b623898e0d7a71a2cdfe
Evan Dancer <evancdancer@gmail.com>
2020-01-19 18:38:44 -0800

    Add default source to payment form

--- app/helpers/builder_helper.rb
+++ app/helpers/builder_helper.rb
@@ -6,4 +6,13 @@ module BuilderHelper
   def month_year
     @postcard.photo.ig_timestamp.strftime('%B %Y')
   end
+
+  def default_payment_source
+    id = @current_user.stripe_customer.default_source
+    payment_sources.find { |x| x.id == id }
+  end
+
+  def payment_sources
+    @current_user.stripe_customer.sources
+  end
 end
