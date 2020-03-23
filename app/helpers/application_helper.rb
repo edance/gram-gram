e1716b0e996c1106729882cd8249579b28fac699
Evan Dancer <evancdancer@gmail.com>
2020-03-22 20:36:36 -0700

    Add cloudinary for photos

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,12 @@
 module ApplicationHelper
+  CLOUDINARY_FETCH = 'https://res.cloudinary.com/gramgram/image/fetch/c_scale,f_auto,w_250/'.freeze
+
   def copyright_year
     DateTime.now.year
+  end
+
+  def cl_image_tag(url, opts)
+    url = ERB::Util.url_encode(url)
+    image_tag("#{CLOUDINARY_FETCH}#{url}", opts)
   end
 end
