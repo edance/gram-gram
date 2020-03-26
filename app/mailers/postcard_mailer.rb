fea2201f979df2b0f249dbb045fdeb14532fa6dc
Evan Dancer <evancdancer@gmail.com>
2020-03-25 20:02:14 -0700

    Add email for out for delivery

--- app/mailers/postcard_mailer.rb
+++ app/mailers/postcard_mailer.rb
@@ -7,4 +7,11 @@ class PostcardMailer < ApplicationMailer
 
     mail(to: @user.email, subject: 'Your photo has been sent!')
   end
+
+  def out_for_delivery
+    @postcard = params[:postcard]
+    @user = postcard.user
+
+    mail(to: @user.email, subject: 'Your photo is out for delivery!')
+  end
 end
