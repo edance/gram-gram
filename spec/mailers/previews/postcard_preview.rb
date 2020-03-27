47fecdb9f0ad25cdd2b6694cc9b1ad0f7bfc7f33
Evan Dancer <evancdancer@gmail.com>
2020-03-26 20:11:26 -0700

    Add html template

--- spec/mailers/previews/postcard_preview.rb
+++ spec/mailers/previews/postcard_preview.rb
@@ -1,4 +1,16 @@
 # Preview all emails at http://localhost:3000/rails/mailers/postcard
 class PostcardPreview < ActionMailer::Preview
+  def receipt
+    PostcardMailer.with(postcard: postcard).receipt
+  end
 
+  def out_for_delivery
+    PostcardMailer.with(postcard: postcard).out_for_delivery
+  end
+
+  private
+
+  def postcard
+    @postcard ||= Postcard.order(:created_at).last
+  end
 end
