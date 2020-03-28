f11e55436c737f8998cc305a1d35e6bf195d3e06
Evan Dancer <evancdancer@gmail.com>
2020-03-27 23:10:49 -0700

    Add lazy-loading and dpi to photo images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,5 @@
 module ApplicationHelper
-  CLOUDINARY_FETCH = 'https://res.cloudinary.com/gramgram/image/fetch/dpr_auto,c_scale,f_auto,w_250/'.freeze
-
   def copyright_year
     DateTime.now.year
-  end
-
-  def cl_image_tag(url, opts)
-    url = ERB::Util.url_encode(url)
-    tag.img(opts.merge(data: { src: "#{CLOUDINARY_FETCH}#{url}" }))
   end
 end
