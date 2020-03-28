fcba03ed1efc81932cbe88b4c94ade0c586301a0
Evan Dancer <evancdancer@gmail.com>
2020-03-27 23:10:49 -0700

    Add lazy-loading and dpi to photo images

--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -1,5 +1,6 @@
 module PhotosHelper
-  def new?(photo)
-    photo.ig_timestamp > 2.weeks.ago
+  def lazy_image_tag(url, opts)
+    opts[:class] = "#{opts[:class]} img-lazy"
+    tag.img(opts.merge(data: { src: url }))
   end
 end
--- app/javascript/components/photo-grid.js
+++ app/javascript/components/photo-grid.js
@@ -1,11 +1,42 @@
+import { u } from 'umbrellajs';
 import Masonry from 'masonry-layout';
+import LazyLoad from 'vanilla-lazyload';
+
+const CLOUDINARY_FETCH = 'https://res.cloudinary.com/gramgram/image/fetch/dpr_auto,c_scale,f_auto,w_250/';
+
+// Add the images have data-src set
+// This function calculates the devicePixelRatio
+function setImageSrc(el) {
+  const devicePixelRatio = Math.ceil(window.devicePixelRatio || 1);
+  const $img = u(el);
+  let src = $img.data('src');
+
+  // Add cloudinary fetch
+  src = `${CLOUDINARY_FETCH}${encodeURIComponent(src)}`;
+
+  // Set the pixel ratio
+  src = src.replace('dpr_auto', `dpr_${devicePixelRatio}.0`);
+
+  $img.data('src', src);
+}
 
 document.addEventListener('turbolinks:load', function() {
   const grid = document.querySelector('.photo-grid');
 
-  new Masonry(grid, {
+  if (!grid) {
+    return;
+  }
+
+  u('.img-fluid').each(setImageSrc);
+
+  const masonry = new Masonry(grid, {
     itemSelector: '.grid-item',
     columnWidth: '.grid-sizer',
     percentPosition: true
+  });
+
+  new LazyLoad({
+    elements_selector: '.img-lazy',
+    callback_loaded: () => masonry.layout(),
   });
 });
