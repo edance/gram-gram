79749c3cad932a333d9ca02b66f3b4ab98e1db68
Evan Dancer <evancdancer@gmail.com>
2020-04-01 21:03:37 -0700

    Allow caching of homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,17 @@
 class HomeController < ApplicationController
+  before_action :redirect_users
+  after_action -> { request.session_options[:skip] = true }
+
   def index
+    expires_in 3.hours,
+               public: true,
+               stale_while_revalidate: 60.seconds,
+               stale_if_error: 5.minutes
+  end
+
+  private
+
+  def redirect_users
     redirect_to photos_path if current_user.present?
   end
 end
