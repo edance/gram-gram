26753c23784473cb5f65159745c609b4a7439fec
Evan Dancer <evancdancer@gmail.com>
2020-04-04 23:09:03 -0700

    Override the sign_out path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  private
+
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(_)
+    new_user_session_path
+  end
 end
