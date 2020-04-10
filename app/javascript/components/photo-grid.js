1f7fe7b8ca99d6286c482cf0f3b6d67fef5bac6a
Evan Dancer <evancdancer@gmail.com>
2020-04-09 21:17:01 -0700

    Add max pixel ratio for cloudinary

--- app/javascript/components/photo-grid.js
+++ app/javascript/components/photo-grid.js
@@ -4,10 +4,15 @@ import LazyLoad from 'vanilla-lazyload';
 
 const CLOUDINARY_FETCH = 'https://res.cloudinary.com/gramgram/image/fetch/dpr_auto,c_scale,f_auto,w_250/';
 
+function pixelRatio() {
+  const ratio = window.devicePixelRatio || 1;
+  return ratio > 1 ? 2 : 1;
+}
+
 // Add the images have data-src set
 // This function calculates the devicePixelRatio
 function setImageSrc(el) {
-  const devicePixelRatio = Math.ceil(window.devicePixelRatio || 1);
+  const devicePixelRatio = pixelRatio();
   const $img = u(el);
   let src = $img.data('src');
 
