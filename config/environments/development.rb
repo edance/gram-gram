a9475445d954b89df4a93ee1687065c977e0377e
Evan Dancer <evancdancer@gmail.com>
2020-04-18 15:46:05 -0700

    Set config to prevent emails in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,9 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
+  # Default to skipping sending emails
+  config.action_mailer.perform_deliveries = false
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
