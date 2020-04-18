31be4af42d310803346dcd2b320ee29ef750aee2
Evan Dancer <evancdancer@gmail.com>
2020-04-18 15:30:45 -0700

    Add delay to load instagram photos

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,10 @@
 class WelcomeController < ApplicationController
   before_action :authenticate_user!
 
-  def loading
-    SaveInstagramPhotosJob.perform_later(current_user)
+  after_action :save_photos, only: :loading
+
+  def save_photos
+    # Wait a few seconds in case the user doesn't have any photos
+    SaveInstagramPhotosJob.set(wait: 2.seconds).perform_later(current_user)
   end
 end
