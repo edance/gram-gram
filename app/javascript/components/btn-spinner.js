4a9d0c015b5ca6c49e0c81dc31966baaf99eae75
Evan Dancer <evancdancer@gmail.com>
2020-04-23 20:10:57 -0700

    Fix invalid payment screen
    Clear server side errors when a user types into the input.
    Stop the spinner button if the user puts in invalid input.

--- app/javascript/components/btn-spinner.js
+++ app/javascript/components/btn-spinner.js
@@ -3,7 +3,10 @@ import { u } from 'umbrellajs';
 function startSpin(button) {
   const $button = u(button);
   const width = $button.size().width;
+  const text = $button.html();
+
   $button.attr('disabled', true);
+  $button.data('text', text);
   $button.attr('style', `width: ${width}px`);
   $button.html(`
     <div class="spinner-border spinner-border-sm" role="status">
@@ -12,10 +15,22 @@ function startSpin(button) {
   `);
 }
 
+function stopSpin(button) {
+  const text = button.dataset.text;
+  button.removeAttribute('disabled');
+  button.innerHTML = text;
+}
+
 document.addEventListener("turbolinks:load", function() {
   u('form').on('submit', (e) => {
     u(e.target)
       .find('button.btn-spinner')
       .each(el => startSpin(el));
+  });
+
+  u('form').on('error', (e) => {
+    u(e.target)
+      .find('button.btn-spinner')
+      .each(el => stopSpin(el));
   });
 });
