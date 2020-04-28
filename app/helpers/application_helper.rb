c21644bdcf9e754e2061361c9dc17bc6b5b4cafb
Evan Dancer <evancdancer@gmail.com>
2020-04-27 21:40:06 -0700

    Fix canonical url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def copyright_year
     DateTime.now.year
   end
+
+  def canonical_url
+    url_for(host: 'www.gramgram.app', protocol: 'https', only_path: false)
+  end
 end
