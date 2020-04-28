8621a1450adb2e892f41f4978f7cf783b350f6a4
Evan Dancer <evancdancer@gmail.com>
2020-04-27 22:10:43 -0700

    Update email template
    Update color to match outline buttons with border-radius and dark border. Update
    font to use the same Poppins font we use everywhere. Make font darker for dark
    mode on phones.

--- spec/mailers/previews/order_preview.rb
+++ spec/mailers/previews/order_preview.rb
@@ -1,4 +1,12 @@
 # Preview all emails at http://localhost:3000/rails/mailers/order
 class OrderPreview < ActionMailer::Preview
+  def receipt
+    OrderMailer.with(order: order).receipt
+  end
 
+  private
+
+  def order
+    @order ||= Order.order(:created_at).last
+  end
 end
