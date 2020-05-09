f15fedeb8677545d83014c58a2f211f6e5f68fa7
Evan Dancer <evancdancer@gmail.com>
2020-05-09 16:25:25 -0700

    Add scroll down arrow for homepage

--- app/javascript/components/homepage-scroll.js
+++ app/javascript/components/homepage-scroll.js
@@ -1,6 +1,10 @@
 import * as ScrollMagic from 'scrollmagic';
 
 document.addEventListener('turbolinks:load', function() {
+  if ($('#preview').length === 0) {
+    return;
+  }
+
 	// init controller
 	var controller = new ScrollMagic.Controller();
 
@@ -8,17 +12,17 @@ document.addEventListener('turbolinks:load', function() {
 	new ScrollMagic.Scene({
     triggerElement: '#preview',
     triggerHook: 0.2,
-    duration: '170%',
+    duration: '150%',
   }).setPin('#preview')
     .addTo(controller);
 
   new ScrollMagic.Scene({
-    triggerElement: "#photo-section",
+    triggerElement: "#photo-text",
   }).setClassToggle("#preview", "active") // add class toggle
     .addTo(controller);
 
   new ScrollMagic.Scene({
-    triggerElement: "#mail-section",
+    triggerElement: "#mail-text",
   }).setClassToggle("#preview", "flip") // add class toggle
     .addTo(controller);
 });
