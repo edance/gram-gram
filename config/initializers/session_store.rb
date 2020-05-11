74b4eb383ef3e9868f003716f27504f3b811511a
Evan Dancer <evancdancer@gmail.com>
2020-05-10 18:46:57 -0700

    Remove session_store initializer

--- config/initializers/session_store.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.config.session_store(
-  :cookie_store,
-  expire_after: 14.days,
-  secure: Rails.env.production?, # flags cookies as secure only in production
-  same_site: :strict,
-  httponly: true # should be true by default for all cookies
-)
