dec482ab530e82daba425eedf85c87f84d36b3f4
Evan Dancer <evancdancer@gmail.com>
2020-07-20 14:59:08 -0700

    Allow skip sync of instagram

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,5 @@
 class PhotosController < ApplicationController
   before_action :authenticate_user!
-  before_action :check_instagram!, only: :index
   before_action :check_sync_at!, only: :index
 
   def index
@@ -16,12 +15,8 @@ class PhotosController < ApplicationController
 
   private
 
-  def check_instagram!
-    redirect_to connect_path unless current_user.instagram?
-  end
-
   def check_sync_at!
-    return if current_user.sync_at.nil?
+    return if current_user.sync_at.nil? || !current_user.instagram?
 
     redirect_to reconnect_path if current_user.sync_at < 1.day.ago
   end
